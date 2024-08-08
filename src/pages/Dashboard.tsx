154515f3f9d7e9a4e84252ec893616b7298573f2
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-08-08 14:59:27 +0530

    Add: Dashboard Component

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,16 @@
+import './Dashboard.css';
+
+const Dashboard =()=>{
+    return(
+      <main>
+        <section>
+            <div>
+                <p>My Keys</p>
+                <div className="divider"></div>
+            </div>
+        </section>
+      </main>
+    )
+}
+
+export default Dashboard;
